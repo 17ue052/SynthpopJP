eb2d57eb47907cea30644cc8e3a301a9ba8e192a
17ue052 <17ue052@mb2.fwu.ac.jp>
2021-02-03 17:22:37 +0900

    change

--- R/syn2.R
+++ R/syn2.R
@@ -12,7 +12,11 @@ syn2 <- function(V,n){
     dplyr::select(c("性別","年齢",V)) -> JPdata2
 
   synthpop::syn(data = JPdata2, k=n, visit.sequence = c("性別","年齢")) -> synJP_list
-  synJP_list$syn -> synJP_df
+  pM.cor <- synJP_list$predictor.matrix
+  pM.cor[c("性別","年齢"),] <- 1
+  pM.cor[,c("性別","年齢")] <- 1
+  synthpop::syn(data = JPdata2, k=500,predictor.matrix = pM.cor) -> synJP_list2
+  synJP_list2$syn -> synJP_df
 
   return(synJP_df)
 }
--- R/syn3.R
+++ R/syn3.R
@@ -12,7 +12,11 @@ syn3 <- function(V,n){
     dplyr::select(c("性別","年齢",V)) -> JPdata2
 
   synthpop::syn(data = JPdata2, k=n, visit.sequence = c("性別","年齢")) -> synJP_list
-  synJP_list$syn %>%
+  pM.cor <- synJP_list$predictor.matrix
+  pM.cor[c("性別","年齢"),] <- 1
+  pM.cor[,c("性別","年齢")] <- 1
+  synthpop::syn(data = JPdata2, k=500,predictor.matrix = pM.cor) -> synJP_list2
+  synJP_list2$syn %>%
     dplyr::mutate(dplyr::sample_n(myoji, n)) %>%
     dplyr::mutate(dplyr::sample_n(onna_namae, n)) %>%
     dplyr::mutate(dplyr::sample_n(otoko_namae, n)) %>%
